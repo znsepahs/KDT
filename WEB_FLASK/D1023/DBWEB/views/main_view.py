996a5fea0c14461e1d6b5263dac87cede28aacb5
znsepahs <lbaskinl31@naver.com>
2024-10-23 15:40:42 +0900

    241023

--- WEB_FLASK/D1023/DBWEB/views/main_view.py
+++ WEB_FLASK/D1023/DBWEB/views/main_view.py
@@ -9,14 +9,22 @@ from DBWEB.models.models import Question
 # Blueprint 인스턴스 생성
 mainBP=Blueprint('MAIN', import_name=__name__, url_prefix='/', template_folder='templates')
 
-# http://localhost:8080/ URL 처리 라우팅 함수 정의
+# http://localhost:5000/ URL 처리 라우팅 함수 정의
 @mainBP.route("/")
 def index():
     return render_template('index.html', name='yunho')
 
-# http://localhost:8080/ URL 처리 라우팅 함수 정의
+# http://localhost:5000/ URL 처리 라우팅 함수 정의
 @mainBP.route("/qlist")
 def printList():
     # DB에서 조회한 결과를 HTML 파일로 전달
     q_list=Question.query.all()
     return render_template('question_list.html', question_list=q_list)
+
+# http://localhost:5000/ URL 처리 라우팅 함수 정의
+@mainBP.route("/qdetail/<int:qid>")
+@mainBP.route("/qdetail/<int:qid>/")
+def questionItem(qid):
+    # DB에서 조회한 1개의 question 인스턴스를 전달
+    q=Question.query.get(qid)
+    return render_template('question_detail.html', question=q)
